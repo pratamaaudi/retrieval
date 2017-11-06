a8fb43dfdc143fb927f15e3b6b874b3d18d6ffa1
Fadhil Amadan <fadhil.amadan@yahoo.co.id>
2017-11-06 23:46:34 +0700

    TFIDF

--- db.php
+++ db.php
@@ -5,5 +5,17 @@ if(mysqli_connect_errno()) {
     echo "Connect Error: " . mysqli_connect_error();
     die("Connect Error: " . mysqli_connect_error());   
 }
-    
+   
+
+$link2 = mysqli_connect("localhost", "root", "", "retrieval2");
+if(mysqli_connect_errno()) {
+    echo "Connect Error: " . mysqli_connect_error();
+    die("Connect Error: " . mysqli_connect_error());   
+}
+
+$link3 = mysqli_connect("localhost", "root", "", "retrieval2");
+if(mysqli_connect_errno()) {
+    echo "Connect Error: " . mysqli_connect_error();
+    die("Connect Error: " . mysqli_connect_error());   
+}
 ?>
